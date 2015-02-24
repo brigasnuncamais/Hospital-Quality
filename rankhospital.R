8cb93519db3a66f1accad30b27b94e02c23734c3
brigasnuncamais <pierre.vettori@aurumviae.com>
2015-02-24 23:06:50 +0100

    best validated

--- rankhospital.R
+++ rankhospital.R
@@ -1,4 +1,4 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = "best") {
 
   ## Read outcome data
   outcomeCM <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
@@ -6,9 +6,10 @@ best <- function(state, outcome) {
   ## Check that state and outcome are valid
   if (is.na(match(state,unique(outcomeCM[,7])))) stop("invalid state")
   if (is.na(match(outcome,c("heart attack","heart failure","pneumonia")))) stop("invalid outcome")
- 
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+  if (is.na(match(num,c("best","worst"))) & is.na(as.integer(num))) stop("invalid num")
+  
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
   options(warn = -1)
   Coloutcome <- switch (outcome, "heart attack" = 11,
                         "heart failure" = 17,
