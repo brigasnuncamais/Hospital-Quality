2d57d1fb59cfde49316a5a6aca97807cb72c1c8b
brigasnuncamais <pierre.vettori@aurumviae.com>
2015-02-25 21:19:29 +0100

    rankall validated online corrected comment in rankhospital

--- rankhospital.R
+++ rankhospital.R
@@ -3,7 +3,7 @@ rankhospital <- function(state, outcome, num = "best") {
   ## Read outcome data
   outcomeCM <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
-  ## Check that state and outcome are valid
+  ## Check that state, outcome and num are valid
   if (is.na(match(state,unique(outcomeCM[,7])))) stop("invalid state")
   if (is.na(match(outcome,c("heart attack","heart failure","pneumonia")))) stop("invalid outcome")
   if (is.na(match(num,c("best","worst"))) & is.na(as.integer(num))) stop("invalid num")
