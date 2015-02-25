ad60401bf20bef0cae440d9f403d530f40cb4997
Pierre Vettori <pierre.vettori@aurumviae.com>
2015-02-25 18:43:30 +0100

    validated online

--- rankhospital.R
+++ rankhospital.R
@@ -1,8 +1,8 @@
 rankhospital <- function(state, outcome, num = "best") {
-
+  
   ## Read outcome data
   outcomeCM <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-
+  
   ## Check that state and outcome are valid
   if (is.na(match(state,unique(outcomeCM[,7])))) stop("invalid state")
   if (is.na(match(outcome,c("heart attack","heart failure","pneumonia")))) stop("invalid outcome")
@@ -17,7 +17,14 @@ rankhospital <- function(state, outcome, num = "best") {
   SubsetOutcomeCM<-subset(outcomeCM,outcomeCM[,7]==state)
   SubsetOutcomeCM[,Coloutcome]<-sapply(SubsetOutcomeCM[,Coloutcome], as.character)
   SubsetOutcomeCM[,Coloutcome]<-sapply(SubsetOutcomeCM[,Coloutcome], as.numeric)
-  minDeath<-min(SubsetOutcomeCM[,Coloutcome], na.rm=TRUE)
-  HospitalRow<-subset(SubsetOutcomeCM, SubsetOutcomeCM[,Coloutcome]==minDeath)
-  HospitalName<-as.character(HospitalRow[order(HospitalRow[,2],2),2])
+  SubsetOutcomeCM = na.omit(SubsetOutcomeCM)
+  nHospital  <-  nrow(SubsetOutcomeCM)
+
+  if (!is.na(as.numeric(num))) {
+    if (num > nHospital) return(NA) else numRank <- num}
+  else {
+    numRank <- switch(num, best = 1, worst = nHospital)}
+  
+  HospitalOrder <- order(SubsetOutcomeCM[, Coloutcome], SubsetOutcomeCM[, 2])
+  HospitalName<-SubsetOutcomeCM[HospitalOrder, ][numRank, 2]
 }
